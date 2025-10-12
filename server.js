c019615533f8b4b4b9838106013bd4c6800fdb77
1redThread <rajrudra535@gmail.com>
2025-10-12 10:48:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -166,6 +166,6 @@ function escapeHtml(s) {
 }
 
 // Start server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 1000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
